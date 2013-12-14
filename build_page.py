b1b56e00000d9ba7e09eb3049bbcb84fbdd074c1
John Dickinson <me@not.mn>
2013-12-13 23:00:51 -0800

    renamed url doc

--- build_page.py
+++ build_page.py
@@ -1,4 +1,4 @@
-url = ''.join(x.strip() for x in open('graph_url').readlines())
+error_url = ''.join(x.strip() for x in open('error_rate_url').readlines())
 
 page = '<html><head><title>Gate Success Rate</title></head><body><img src="' \
        '%s"></body></html>' % url
