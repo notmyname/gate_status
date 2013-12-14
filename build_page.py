85b5b0d497a565833929d61fb8587b6d03e1cf06
John Dickinson <me@not.mn>
2013-12-13 23:25:05 -0800

    attempt at a gate pressure stat

--- build_page.py
+++ build_page.py
@@ -1,7 +1,9 @@
 error_url = ''.join(x.strip() for x in open('error_rate_url').readlines())
+#pressure_url = ''.join(x.strip() for x in open('gate_pressure_url').readlines())
 
 page = '<html><head><title>Gate Success Rate</title></head><body><img src="' \
-       '%s"></body></html>' % url
+       '%s"></body></html>' % error_url
+#       '%s"><br><img src="%s"></body></html>' % (error_url, pressure_url)
 
 with open('gate_status.html', 'wb') as f:
     f.write(page)
