2bb53515534c397ccee32b2034223e49a3abbfb3
John Dickinson <me@not.mn>
2013-12-17 00:39:17 -0800

    updated explanation, added source ref

--- build_page.py
+++ build_page.py
@@ -6,7 +6,7 @@ msg = '''
 The "Patch Pass Chance" above (the bold red line) is the chance that
 a newly submitted patch has of passing the six tracked gate jobs,
 optimistically assuming that the patch is perfect and introduces no bugs. In
-other words, if you submit a new patch to the jenkins gate queue&mdash;whether
+other words, if you submit a new patch to the Jenkins gate queue&mdash;whether
 a major new feature or a trivial doc spelling error&mdash;the "Patch Pass
 Chance" is the starting point for the likelihood of that patch being merged
 into master.
@@ -15,7 +15,7 @@ into master.
 The depth of the gate queue is also tracked. This allows two things: one, a
 comparison of the patch pass chance to the depth of the queue (eg "Is the
 patch pass chance high because the queue is empty?") and two, the ability to
-calculate the probablity that the current gate queue will clear. To calculate
+calculate the probability that the current gate queue will clear. To calculate
 the chance that the gate queue will clear, take the decimal representation of
 the patch pass chance and raise it to the depth of the gate queue. For example,
 a gate queue 10 deep, when combined with a patch pass chance of 85% has a
@@ -31,6 +31,14 @@ unsuccessful runs in that time block. The "Patch Pass Chance" is the
 multiplication of the gate job pass percentages. Because each gate job failure
 is independent (ie a patch will fail if any of the gate jobs fails), this
 calculation is an accurate representation of the chance a patch has of landing.
+Missing data points in the data (ie nothing happening) is optimistically
+counted as a 100% pass rate. Therefore the "Pass Pass Chance" calculated is
+the absolute best-case scenario, based on the status of the six tracked jobs.
+</p>
+<hr/>
+<p style="font-size: 80%">Source for generating this page is at
+<a href="https://github.com/notmyname/gate_status">
+https://github.com/notmyname/gate_status</a>
 </p>
 '''
 
